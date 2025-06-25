912fe7fa2266f5ba7492319ce655f464433e5b46
Meerthika <meerthikasrrajendrans@gmail.com>
2025-06-25 10:01:23 +0530

    [Add] produceMessage function

--- partitioning/include/producer.h
+++ partitioning/include/producer.h
@@ -1,4 +1,4 @@
 #pragma once
 #include "broker.h"
 
-void produce_message(Broker* broker, char* topic_name, char* message)
+void produce_message(Broker* broker, char* topic_name, char* message);
---
+++ partitioning/src/producer.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/producer.h"
+
+void produce_message(Broker* broker, char* topic_name, char* message){
+    Topic *topic = get_topic(broker, topic_name);
+    if (!topic) {
+        printf("❌ Topic %s not found.\n", topic_name);
+        return;
+    }
+
+    int index = topic->rr_index % topic->partition_count;
+    Partition *p = &topic->partitions[index];
+
+    if(p->count < MAX_MESSAGES){
+        p->messages[p->count++] = strdup(message);
+        printf("Message %s sent to topic %s, partition %d\n", message, topic_name, index);
+        topic->rr_index = (topic->rr_index +1) % (topic->partition_count);
+    }else{
+        printf("Partition %d of topic %s is full", index,topic_name);
+    }
+}
