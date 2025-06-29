a02ed51e5424372a03f884687bd45feb48fabedf
Meerthika <meerthikasrrajendrans@gmail.com>
2025-06-29 17:51:46 +0530

    [Add] implement mmap function in mapper.c

---
+++ mmap/src/mapper.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include "../include/mapper.h"
+
+void read_log_with_mmap(char* filepath){
+    int fd = open(filepath, O_RDONLY);
+    if(fd <0){
+        perror("open error");
+        return;
+    }
+
+    struct stat st;
+    fstat(fd, &st);
+
+    size_t length = st.st_size;
+    char *mapped = mmap(NULL, length, PROT_READ, MAP_SHARED, 0);
+    if(mapped == MAP_FAILED){
+        perror("map error");
+        close(fd);
+        return;
+    }
+
+    printf("Readfile using mmap: \n");
+
+    fwrite(mapped, 1, length, stdout);
+    munmap(mapped, length);
+    close(fd);
+}
