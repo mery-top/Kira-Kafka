4a288708784673a2838644d91064ea052c8f85b5
Meerthika <meerthikasrrajendrans@gmail.com>
2025-06-29 20:08:37 +0530

    [Add] bind_thread_to_core function in threadmap.c

---
+++ threadCore/src/threadmap.c
@@ -0,0 +1,29 @@
+/*
+--------------------------------------------------------------
+Note:
+CAN ONLY RUN ON LINUX
+DIRECT THREAD TO CORE SCHEDULER IS NOT AVAILABLE ON MAC
+--------------------------------------------------------------
+*/
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sched.h>
+#include <stdlib.h>
+#include <time.h>
+#include "../include/threadmap.h"
+
+void bind_thread_to_core(int core_id){
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    CPU_SET(core_id, &cpuset);
+
+    pthread_t current = pthread_self();
+    if(pthread_setaffinity_np(current, sizeof(cpu_set_t), &cpuset)!=0){
+        perror("setaffinity error");
+    }else{
+        prinf("Thread %lu bound to core %d", current, core_id);
+    }
+}
